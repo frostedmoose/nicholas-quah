c6b0e4d3a19b1b2cd9bb82fe5a54623c3bd65d71
Nicholas Quah <nwquah@gmail.com>
2013-10-23 13:08:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NicholasQuah::Application.config.secret_key_base = 'c3e80c348bde1cbe7e53d637fd27fe9c4cbaf1116020d1de64705963deaf24856bf11602321148391a717f997718b5ad7b57a7e081e2d85db6dd2208c03d246e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NicholasQuah::Application.config.session_store :cookie_store, key: '_nicholas-quah_session'
